025b0f8e7efc57902bb4eeb3cdec7b97aa189c36
Mark <mwemekamp@gmail.com>
2020-10-23 10:47:50 +0200

    Added score threshold setting

--- RecaptchaV3/Forms/Recaptcha3.cs
+++ RecaptchaV3/Forms/Recaptcha3.cs
@@ -1,15 +1,15 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Web;
+using Newtonsoft.Json;
 using Umbraco.Forms.Core;
 using Umbraco.Forms.Core.Attributes;
 using Umbraco.Forms.Core.Data.Storage;
 using Umbraco.Forms.Core.Enums;
 using Umbraco.Forms.Core.Models;
 
-namespace Recaptcha.Forms
+namespace RecaptchaV3.Forms
 {
     public sealed class Recaptcha3 : FieldType
     {
@@ -17,7 +17,7 @@ namespace Recaptcha.Forms
         {
             Id = new Guid("08b8057f-06c9-4ca5-8a42-fd1fc2a46eff");
             Name = "Recaptcha3";
-            Description = "Render a Recaptcha v3.";
+            Description = "Adds a hidden Recaptcha v3 form field to your Umbraco form.";
             Icon = "icon-eye";
             DataType = FieldDataType.String;
             SortOrder = 10;
@@ -25,8 +25,11 @@ namespace Recaptcha.Forms
             HideLabel = true;
         }
 
-        [Setting("Additional Settings", PreValues = "homepage,login,social,e-commerce", Description = "Use case", View = "Dropdownlist")]
-        public string AdditionalSettings { get; set; }
+        [Setting("Use case", PreValues = "homepage,login,social,e-commerce", Description = "How is the form used?", View = "Dropdownlist")]
+        public string UseCase { get; set; }
+
+        [Setting("Score Threshold", Description = "Score threshold value, when left empty 0.5 is used", View = "TextField")]
+        public string ScoreThreshold { get; set; }
 
         public override IEnumerable<string> ValidateField(Form form, Field field, IEnumerable<object> postedValues, HttpContextBase context, IFormStorage formStorage)
         {
@@ -37,7 +40,15 @@ namespace Recaptcha.Forms
             var jsonResult = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, token));
             var obj = JsonConvert.DeserializeObject<CaptchaResponse>(jsonResult);
 
-            if (!obj.Success)
+            double scoreThreshold;
+            double.TryParse(ScoreThreshold, out scoreThreshold);
+
+            if (scoreThreshold <= 0 || scoreThreshold > 1)
+            {
+                scoreThreshold = 0.5;
+            }
+
+            if (!obj.Success || obj.Score < scoreThreshold)
             {
                 returnStrings.Add("Recaptcha Failed. Try again!");
             }
@@ -49,5 +60,6 @@ namespace Recaptcha.Forms
     public class CaptchaResponse
     {
         public bool Success { set; get; }
+        public double Score { get; set; }
     }
 }
